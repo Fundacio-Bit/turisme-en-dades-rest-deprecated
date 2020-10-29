472e5f43e6d556a86594d68ea0fc326ce88499b7
Esteve Llado <ellado@fundaciobit.org>
2020-10-29 10:42:07 +0100

    Added JSON Schema for data grids.

---
+++ rest-api/schemas/dataGridSchema.js
@@ -0,0 +1,45 @@
+module.exports = {
+  $schema: 'http://json-schema.org/draft-07/schema#',
+
+  definitions: {
+    translation: {
+      type: 'object',
+      properties: {
+        es: { type: 'string' },
+        ca: { type: 'string' },
+        en: { type: 'string' },
+        de: { type: 'string' }
+      }
+    }
+  },
+
+  type: 'object',
+  properties: {
+    title: { $ref: '#/definitions/translation' },
+    description: { $ref: '#/definitions/translation' },
+    columns: {
+      type: 'array',
+      items: { $ref: '#/definitions/translation' }
+    },
+    rows: {
+      type: 'array',
+      items: {
+        type: 'object',
+        properties: {
+          name: { $ref: '#/definitions/translation' },
+          values: {
+            type: 'array',
+            items: { type: 'number' }
+          }
+        },
+        required: ['name', 'values']
+      }
+    },
+    notes: {
+      type: 'array',
+      items: { $ref: '#/definitions/translation' }
+    }
+  },
+  required: ['title', 'columns', 'rows'],
+  additionalProperties: false
+}
